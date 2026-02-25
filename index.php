32828d7da668be209e87444c1c4841c3f5e07500
nkt18 <nitint8350@gmail.com>
2026-02-26 00:17:45 +0530

    Final submission setup: added .env support, gitignore and documentation

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+header("Location: src/auth/login.php");
+exit;
--- src/config/database.php
+++ src/config/database.php
@@ -2,10 +2,40 @@
 
 class Database {
 
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "worknest_db";
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+
+    public function __construct() {
+
+        /* Try loading .env file */
+
+        $envPath = __DIR__ . "/../../.env";
+
+        if(file_exists($envPath)){
+
+            $env = parse_ini_file($envPath);
+
+            $this->host = $env['DB_HOST'] ?? "localhost";
+            $this->username = $env['DB_USER'] ?? "root";
+            $this->password = $env['DB_PASS'] ?? "";
+            $this->database = $env['DB_NAME'] ?? "worknest_db";
+
+        }
+
+        else{
+
+            $this->host = "localhost";
+            $this->username = "root";
+            $this->password = "";
+            $this->database = "worknest_db";
+
+        }
+
+    }
+
+
 
     public function connect() {
 
@@ -17,9 +47,12 @@ class Database {
         );
 
         if ($conn->connect_error) {
-            die("Database Connection Failed: " . $conn->connect_error);
+
+            die("Database Connection Failed");
+
         }
 
         return $conn;
     }
+
 }
